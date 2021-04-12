04d188256c0495d38980da16da1bf05c82ec805d
Ryabikovalex <ryabikov.alex@gmail.com>
2021-04-12 23:10:54 +0300

    Added base class for forms

---
+++ ProjectForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Hotel_SA
+{
+    public class ProjectForm : Form
+    {
+        protected ProjectForm ParentF { get; set; }
+        public virtual void enableElements(){}
+        public virtual void disableElements(){}
+       
+
+        protected void Event_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ParentF.enableElements();
+            ParentF.Show();
+            ParentF.Activate();
+        }
+    }
+}
